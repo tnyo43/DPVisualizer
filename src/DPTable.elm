59222306002ec6d9fe52fb72e8b9a2c146419b3f
tyno43 <tnyo43@gmail.com>
2020-03-07 01:19:34 +0900

    add apply init recursive formula function

--- src/DPTable.elm
+++ src/DPTable.elm
@@ -1,7 +1,8 @@
 module DPTable exposing (..)
 
 import Array exposing (Array)
-
+import Expr exposing (Term(..))
+import RecursionFormula exposing (..)
 
 type alias Table =
     { h : Int
@@ -28,3 +29,40 @@ updateSize h w table =
             )
     in
     { table | h = h, w = w, t = t }
+
+
+editTable : Int -> Int -> Int -> Table -> Table
+editTable r c n tbl =
+    case Array.get r tbl.t of
+        Just row ->
+            { tbl | t = Array.set r ( Array.set c n row ) tbl.t }
+        Nothing ->
+            tbl
+
+
+apply : Table -> FFixed -> Table
+apply tbl frm =
+    let
+        ( vw, idxW ) =
+            case frm.arg2 of
+                Var vw_ ->
+                    ( vw_, List.repeat tbl.w vw_ |> List.indexedMap (\i v -> (v, i)) )
+                Con w ->
+                    ( "_", List.singleton ("_", w) )
+                _ -> ( "_", [] )
+        idx =
+            case frm.arg1 of
+                Var vh ->
+                    if vh == vw
+                    then List.repeat ( min tbl.h tbl.w ) vh |> List.indexedMap (\i v -> ( (v, i), (v, i) ))
+                    else List.indexedMap (\h row -> List.repeat tbl.h row |> List.indexedMap (\i c -> ( (vh, i), c ))) idxW |> List.concat
+                Con h ->
+                    List.map (\tup -> ( ("_", h), tup )) idxW
+                _ -> []
+        val =
+            case frm.term of
+                Con x -> x
+                _ -> 0 -- TODO: implement not constant case
+    in
+    List.foldl (\((_, h), (_, w)) accTbl -> editTable h w val accTbl) tbl idx
+            
---
+++ tests/ExDPTable.elm
@@ -0,0 +1,53 @@
+module ExDPTable exposing (..)
+
+import Array exposing (Array)
+import DPTable exposing (..)
+import Expect exposing (Expectation)
+import Expr exposing (Term(..))
+import Fuzz exposing (Fuzzer, int, list, string)
+import RecursionFormula exposing (..)
+import Test exposing (..)
+
+
+initTable_5_5 : () -> Table
+initTable_5_5 _ =
+    initTable 5 5
+
+
+testApplyDPInit : String -> FFixed -> Table -> Test
+testApplyDPInit str frm expected =
+    test str <|
+        \_ -> Expect.equal ( apply (initTable_5_5 ()) frm ) expected
+
+
+editMultipleCels : Table -> List (Int, Int, Int) -> Table
+editMultipleCels =
+    List.foldl (\(i, j, n) t -> editTable i j n t)
+
+
+suite : Test
+suite =
+    describe "DP Table"
+        [ describe "DP初期条件で初期化"
+            [ testApplyDPInit
+                "dp[0][0] = 1"
+                ( FFixed (Con 0) (Con 0) (Con 1) )
+                ( editMultipleCels (initTable_5_5 ()) [(0,0,1)] )
+            , testApplyDPInit
+                "dp[i][i] = 1は対角上が1"
+                ( FFixed (Var "i") (Var "i") (Con 1) )
+                ( editMultipleCels (initTable_5_5 ()) [(0,0,1), (1,1,1), (2,2,1), (3,3,1), (4,4,1)] )
+            , testApplyDPInit
+                "dp[i][0] = 1はすべてのiに対して適用される"
+                ( FFixed (Var "i") (Con 0) (Con 1) )
+                ( editMultipleCels (initTable_5_5 ()) [(0,0,1), (1,0,1), (2,0,1), (3,0,1), (4,0,1)] )
+            , testApplyDPInit
+                "dp[0][w] = 1はすべてのwに対して適用される"
+                ( FFixed (Con 2) (Var "w") (Con 10) )
+                ( editMultipleCels (initTable_5_5 ()) [(2,0,10), (2,1,10), (2,2,10), (2,3,10), (2,4,10)] )
+            , testApplyDPInit
+                "dp[i][j] = 1はすべて1"
+                ( FFixed (Var "i") (Var "j") (Con 1) )
+                ( editMultipleCels (initTable_5_5 ()) (List.range 0 25 |> List.map (\x -> (x // 5, modBy 5 x, 1))) )
+            ]
+        ]
